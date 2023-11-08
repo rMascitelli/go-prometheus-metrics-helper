efc42f5f4eddd96598981216cad2c20d301c48d6
Robert Mascitelli <robertmascitelli@gmail.com>
2023-11-07 20:59:13 -0500

    Now supporting Gauges and Counters

--- client.go
+++ client.go
@@ -1,35 +1,66 @@
 package main
 
 import (
-	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"net/http"
 )
 
 type PrometheusClient struct {
-	Metrics map[string]prometheus.Counter
+	gauges   map[string]*prometheus.GaugeVec
+	counters map[string]*prometheus.CounterVec
 }
 
 func NewPrometheusClient() PrometheusClient {
 	http.Handle("/metrics", promhttp.Handler())
 	return PrometheusClient{
-		Metrics: make(map[string]prometheus.Counter),
+		gauges:   make(map[string]*prometheus.GaugeVec),
+		counters: make(map[string]*prometheus.CounterVec),
 	}
 }
 
-func (p *PrometheusClient) AddNewCounter(name string, desc string) {
-	newCounterPtr := promauto.NewCounter(
-		prometheus.CounterOpts{
-			Name: name, // metric name
-			Help: desc,
-		},
-	)
-	fmt.Printf("metrics: %+v\n", p.Metrics)
-	p.Metrics[name] = newCounterPtr
+func (p *PrometheusClient) AddNewGauge(metricname string, description string) {
+	if _, ok := p.gauges[metricname]; !ok {
+		p.gauges[metricname] = promauto.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: metricname, // metric name
+				Help: description,
+			},
+			[]string{"hostname"},
+		)
+	} else {
+		log.Printf("Metric %s already exists\n", metricname)
+	}
 }
 
-func (p *PrometheusClient) IncrementMetric(name string) {
-	p.Metrics[name].Inc()
+func (p *PrometheusClient) AddNewCounter(metricname string, description string) {
+	if _, ok := p.gauges[metricname]; !ok {
+		p.counters[metricname] = promauto.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: metricname, // metric name
+				Help: description,
+			},
+			[]string{"hostname"},
+		)
+	} else {
+		log.Printf("Metric %s already exists\n", metricname)
+	}
+}
+
+func (p *PrometheusClient) IncrementCounter(metricname string, hostname string) {
+	if counterVec, ok := p.counters[metricname]; ok {
+		counterVec.WithLabelValues(hostname).Inc()
+	} else {
+		log.Printf("Metric %s doesnt exist\n", metricname)
+	}
+}
+
+func (p *PrometheusClient) SetGaugeVal(metricname string, hostname string, gaugeval float64) {
+	if gaugeVec, ok := p.gauges[metricname]; ok {
+		gaugeVec.WithLabelValues(hostname).Set(gaugeval)
+	} else {
+		log.Printf("Metric %s doesnt exist\n", metricname)
+	}
 }
--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
 	p := NewPrometheusClient()
-	p.AddNewCounter("test_metric", "Random test metric")
+	hostname := "randomServiceName"
+	p.AddNewCounter("test_counter", "Testing out a counter")
+	p.AddNewGauge("test_gauge", "Testing out a gauge")
 
 	go func() {
+		log.Println("Hosting metrics at localhost:2112")
 		http.ListenAndServe(":2112", nil)
 	}()
 	for {
-		p.IncrementMetric("test_metric")
+		randfloat := float64(time.Now().Unix()) / 13
+		p.IncrementCounter("test_counter", hostname)
+		p.SetGaugeVal("test_gauge", hostname, randfloat)
 		time.Sleep(time.Second * 2)
 	}
 }
