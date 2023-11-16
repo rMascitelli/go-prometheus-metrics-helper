2cbfdfd266eb15087999f455232e4220030f5efd
Robert Mascitelli <robertmascitelli@gmail.com>
2023-11-15 19:04:44 -0500

    cleanup

---
+++ main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	gopro "github.com/rMascitelli/go-prometheus-metrics-helper/clien"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	p := gopro.client.NewPrometheusClient()
+	servicename := "randomServiceName"
+	p.AddNewCounter("test_counter", "Testing out a counter")
+	p.AddNewGauge("test_gauge", "Testing out a gauge")
+
+	go func() {
+		log.Println("Hosting metrics at localhost:2112")
+		http.ListenAndServe(":2112", nil)
+	}()
+	for {
+		randfloat := float64(time.Now().Unix()) / 13
+		p.IncrementCounter("test_counter", servicename)
+		p.SetGaugeVal("test_gauge", servicename, randfloat)
+		time.Sleep(time.Second * 2)
+	}
+}
